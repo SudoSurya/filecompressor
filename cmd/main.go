4bb7cd76c5e65973aebe0317e2c1b63b9d5e40ef
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2024-02-19 18:00:49 +0530

    feat: lipgloss for stying

--- cmd/main.go
+++ cmd/main.go
@@ -5,85 +5,103 @@ import (
 	"image/jpeg"
 	"os"
 	"path/filepath"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 type FileInfo struct {
 	name     string
 	size     int64
-	contents string
 	filetype string
 }
+
+var titleStyle = lipgloss.NewStyle().
+	Bold(true).
+	Margin(0, 1, 0, 1)
+var itemStyle = lipgloss.NewStyle().
+	Bold(true).
+	Margin(0, 1, 0, 1)
 
 func main() {
 	args := os.Args
 
 	FILENAME := args[1]
 
-	fmt.Println(filepath.Ext(FILENAME))
-
 	switch filepath.Ext(FILENAME) {
 	case ".jpeg", ".jpg":
-        err := compressJPEG(FILENAME)
-        if err != nil {
-            fmt.Println(err)
-        }
+		originalFile, compressFile, err := compressJPEG(FILENAME)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(lipgloss.NewStyle().Align(lipgloss.Center).Background(lipgloss.Color("#1fb55b")).Render("Original File Info"))
+		renderFileInfo(originalFile)
+		fmt.Println(lipgloss.NewStyle().Align(lipgloss.Center).Background(lipgloss.Color("#f70202")).Render("Compressed File Info"))
+        renderFileInfo(compressFile)
+	case ".pdf":
+		fmt.Println("PDF")
 	}
 }
 
-func compressJPEG(FILENAME string) error {
+func renderFileInfo(fileInfo FileInfo) {
+	fmt.Print("\n")
+	fmt.Println(titleStyle.Render("Name: ") + itemStyle.Render(fileInfo.name))
+	fmt.Println(titleStyle.Render("Size: ") + itemStyle.Render(fmt.Sprintf("%d bytes", fileInfo.size)))
+	fmt.Println(titleStyle.Render("Type: ") + itemStyle.Render(fileInfo.filetype))
+	fmt.Print("\n")
+}
+
+func compressJPEG(FILENAME string) (FileInfo, FileInfo, error) {
 	file, err := os.Open(FILENAME)
 	if err != nil {
-		return err
+		return FileInfo{}, FileInfo{}, err
 	}
 	defer file.Close()
 
 	fileStat, err := file.Stat()
 
 	if err != nil {
-		return err
+		return FileInfo{}, FileInfo{}, err
 	}
 	originalFILE := FileInfo{
 		name:     FILENAME,
 		size:     fileStat.Size(),
 		filetype: filepath.Ext(FILENAME),
 	}
-	fmt.Println(originalFILE)
 
 	img, err := jpeg.Decode(file)
 
 	if err != nil {
-		return err
+		return FileInfo{}, FileInfo{}, err
 	}
 
-	newFILE, err := os.Create("compressed_" + FILENAME)
+	compressedFILE, err := os.Create("compressed_" + FILENAME)
 
 	if err != nil {
-		return err
+		return FileInfo{}, FileInfo{}, err
 	}
 
-	defer newFILE.Close()
+	defer compressedFILE.Close()
 
-	err = jpeg.Encode(newFILE, img, &jpeg.Options{Quality: 70})
+	err = jpeg.Encode(compressedFILE, img, &jpeg.Options{Quality: 70})
 
 	if err != nil {
-		return err
+		return FileInfo{}, FileInfo{}, err
 	}
 
-	newFILEStat, err := newFILE.Stat()
+	compressedFILEStat, err := compressedFILE.Stat()
 
 	if err != nil {
-		return err
+		return FileInfo{}, FileInfo{}, err
 	}
 
-	newFILEINFO := FileInfo{
+	compressedFILEINFO := FileInfo{
 		name:     "compressed_" + FILENAME,
-		size:     newFILEStat.Size(),
+		size:     compressedFILEStat.Size(),
 		filetype: filepath.Ext(FILENAME),
 	}
 
-	fmt.Println(newFILEINFO)
 
-	return nil
+	return originalFILE, compressedFILEINFO, nil
 }
 
 /* func readFileContents(FILENAME string) (FileInfo, error) {
---
+++ utils/utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "strconv"
+
+func IntToStr(i int64) string {
+    return strconv.FormatInt(i, 10)
+}
